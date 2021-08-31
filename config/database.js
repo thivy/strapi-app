ce107b389fde254fa6a40bfb92d078c487e9542b
thivy <thivyakanth@gmail.com>
2021-08-31 16:11:23 +1000

    add production variables

--- config/database.js
+++ config/database.js
@@ -4,14 +4,12 @@ module.exports = ({ env }) => ({
     default: {
       connector: "bookshelf",
       settings: {
-        client: "mysql",
-        host: env("DATABASE_HOST", "strapi"),
-        port: env.int("DATABASE_PORT", 3306),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
-      options: {},
+      options: {
+        useNullAsDefault: true,
+      },
     },
   },
 });
